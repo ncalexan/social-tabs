11b52837d5de685f63642c3793da5c87a91c662a
Nick Alexander <nalexander@mozilla.com>
2013-02-15 11:34:37 -0800

    Standardize `log` function in sidebar.js and worker.js.

--- sidebar.js
+++ sidebar.js
@@ -1,7 +1,15 @@
+var LOG_TAG = "sidebar";
+function log(msg) {
+  var s = new Date().toISOString() + ": " + LOG_TAG + " " + msg;
+  dump(s + "\n");
+  try {
+    console.log(s);
+  } catch (e) {}
+}
 
 function onLoad() {
   var worker = navigator.mozSocial.getWorker();
-  $("body").css("background-color", worker? "green": "red")
+  $("body").css("background-color", worker? "green": "red");
   $("#domain").text(location.host);
   var data = document.cookie.split("=",2)[1];
   userIsConnected(JSON.parse(data));
@@ -33,7 +41,7 @@ function openDataPanel(event) {
   // currently cant do this
   var url = "data:text/html,%3Chtml%3E%3Cbody%3E%3Cp%3EInline%20data%3C%2Fp%3E%3C%2Fbody%3E%3C%2Fhtml%3E";
   navigator.mozSocial.openPanel(url, event.clientY, function(win) {
-	dump("window is opened "+win+"\n");
+	log("window is opened "+win);
   });
 }
 
@@ -81,22 +89,22 @@ var messageHandlers = {
 };
 
 navigator.mozSocial.getWorker().port.onmessage = function onmessage(e) {
-  dump("SIDEBAR Got message: " + e.data.topic + " " + e.data.data +"\n");
+  log("Got message: " + e.data.topic + " " + e.data.data );
   var topic = e.data.topic;
   var data = e.data.data;
 
   if (messageHandlers[topic]) {
     messageHandlers[topic](data);
-    dump("SIDEBAR Handled message: " + topic + "\n");
+    log("Handled message: " + topic);
     return;
   }
 
   if (topic && topic == "social.port-closing") {
-    dump("SIDEBAR port has closed\n");
+    log("port has closed");
     return;
   }
 
-  dump("SIDEBAR Unhandled message: " + e.data.topic + " " + e.data.data +"\n");
+  log("Unhandled message: " + e.data.topic + " " + e.data.data );
 };
 
 // here we ask the worker to reload itself.  The worker will send a reload
@@ -114,7 +122,7 @@ function openPanel(button) {
   var div = document.getElementById("hovertest");
   var baseOffset = button.offsetTop - div.scrollTop + (button.clientHeight/2);
   navigator.mozSocial.openPanel("./flyout.html#"+button.value, baseOffset, function(win) {
-    dump("window is opened "+win+"\n");
+    log("window is opened "+win);
     panelWin = win;
   });
 }
@@ -130,20 +138,20 @@ function closePanel() {
 var chatWin;
 function openChat(event) {
   navigator.mozSocial.openChatWindow("./chatWindow.html?id="+(chatters++), function(win) {
-	dump("chat window is opened "+win+"\n");
+	log("chat window is opened "+win);
     chatWin = win;
   });
 }
 
 // just some test debug output for some events
 window.addEventListener("scroll", function(e) {
-  dump("scrolling sidebar...\n");
+  log("scrolling sidebar...");
 }, false);
 window.addEventListener("socialFrameShow", function(e) {
-  dump("status window has been shown, visibility is "+document.visibilityState+" or "+navigator.mozSocial.isVisible+"\n");
+  log("status window has been shown, visibility is "+document.visibilityState+" or "+navigator.mozSocial.isVisible);
 }, false);
 window.addEventListener("socialFrameHide", function(e) {
-  dump("status window has been hidden, visibility is "+document.visibilityState+" or "+navigator.mozSocial.isVisible+"\n");
+  log("status window has been hidden, visibility is "+document.visibilityState+" or "+navigator.mozSocial.isVisible);
 }, false);
 
 // this notify function is used for manual testing.  We tell the worker to
@@ -179,7 +187,7 @@ function notify(type) {
 var localDeviceInfo = null; // { profileID: 'localhost' };
 
 function renderDevice(deviceName, online, tabs) {
-  dump("device " + deviceName + " (online: " + online + ")\n");
+  log("device " + deviceName + " (online: " + online + ")");
 
   var dul = $("#templates>.device-entry").clone();
   dul.find("span.device-name").text(deviceName);
@@ -194,7 +202,7 @@ function renderDevice(deviceName, online, tabs) {
 
   var tul = dul.find("ul.device-tabs");
   tabs.forEach(function(tab) {
-    dump("tab " + tab.title + "\n");
+    log("tab " + tab.title);
     var title = tab.title || "(no title)";
     var t = $("#templates>.tab-entry").clone();
     t.find("a").attr("href", tab.url).attr("target", "_blank");
@@ -227,7 +235,7 @@ function renderTabs(data) {
     var tabs = data[deviceName].tabs || [];
 
     var dul = renderDevice(deviceName, online, tabs);
-    dump("" + dul);
+    log("" + dul);
 
     ul.append(dul);
   });
@@ -240,7 +248,7 @@ messageHandlers["tabs"] = renderTabs;
  * events.
  */
 function workerTabsRequestEvents() {
-  dump("workerTabsRequestEvents");
+  log("workerTabsRequestEvents");
   var worker = navigator.mozSocial.getWorker();
   worker.port.postMessage({topic: "worker.tabs.request-events", data: true});
 }
@@ -250,19 +258,19 @@ function workerTabsRequestEvents() {
  * events.
  */
 function workerTabsFetchAll() {
-  dump("workerTabsFetchAll");
+  log("workerTabsFetchAll");
   var worker = navigator.mozSocial.getWorker();
   worker.port.postMessage({topic: "worker.tabs.fetchAll", data: true});
 }
 
 messageHandlers["social.tabs.fetchAll-response"] = function(data) {
-  dump("social.tabs.fetchAll-response " + JSON.stringify(data));
+  log("social.tabs.fetchAll-response " + JSON.stringify(data));
 };
 
 /**
  * Include this machine's name in tabs header.
  */
 messageHandlers["social.tabs.request-events-response"] = function(data) {
-  dump("social.tabs.request-events-response " + JSON.stringify(data));
+  log("social.tabs.request-events-response " + JSON.stringify(data));
   localDeviceInfo = data.localDeviceInfo;
 };
