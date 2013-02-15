7d6038ba5be9d9c0b336d2e74f63bc17c1d2d4f0
Nick Alexander <nalexander@mozilla.com>
2013-02-15 09:50:38 -0800

    Kill trailing whitespace.

--- worker.js
+++ worker.js
@@ -46,7 +46,7 @@ onoffline = function() {
 onconnect = function(e) {
   try {
     var port = e.ports[0];
-    
+
     // this is our basic message handler
     port.onmessage = function(e) {
       //log("worker onmessage: " + JSON.stringify(e.data));
@@ -107,14 +107,14 @@ var localDeviceInfo = null;
 // relying on cookies as part of our demo login functionality, we need to
 // poll the cookies for changes. We start that polling during social.initialize
 function checkCookies() {
-  apiPort.postMessage({topic: 'social.cookies-get'});  
+  apiPort.postMessage({topic: 'social.cookies-get'});
 }
 
 // Messages from the sidebar and chat windows:
 var handlers = {
   // worker.reload is our own message and is not defined by socialapi.  Our
   // test sidebar can send this, letting us know to force a reload of
-  // this worker script.  
+  // this worker script.
   'worker.reload': function(port, msg) {
     clearInterval(checkCookies);
     broadcast(msg.topic, msg.data);
@@ -130,7 +130,7 @@ var handlers = {
     setInterval(checkCookies, 1000);
   },
 
-  // our content (sidebar, etc) can request broadcast messages. 
+  // our content (sidebar, etc) can request broadcast messages.
   'broadcast.listen': function(port, data) {
     if (data) {
       _broadcastReceivers.push(port);
@@ -169,7 +169,7 @@ var handlers = {
                 'sharedLabel': "Love It!",
                 'unsharedLabel': "Hate It!",
                 "unshareLabel": "dont do this",
-                "portraitLabel": "dont do this", 
+                "portraitLabel": "dont do this",
                 "unshareConfirmLabel": "dont do this",
                 "unshareConfirmAccessKey": "dont do this",
                 "unshareCancelLabel": "dont do this",
