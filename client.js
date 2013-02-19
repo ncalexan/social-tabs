70ae8511687de766cd5e0fe2996bdd85d5514f9f
Nick Alexander <nalexander@mozilla.com>
2013-02-19 14:49:32 -0800

    Factor out Client.{_get,_put}JSON and add Client.init.
    This paves the way for testing the client against a mock server.

--- client.js
+++ client.js
@@ -15,35 +15,11 @@
     return Client;
   };
 
-  Client.setTabs = function setTabs(user, device, data, cb) {
-    // Temporarily, tabs needs to be a string.
-    var tabs = { tabs: JSON.stringify(data) };
-
-    var url = "/tabs/" + user + "/" + device;
-
+  // Not exported.
+  function _getJSON(url, cb) {
     var req = new XMLHttpRequest();
 
-    req.onreadystatechange=function() {
-      if (req.readyState == 4) {
-        if (req.status == 200) {
-          cb(null);
-        } else {
-          cb(req.status);
-        }
-      }
-    };
-
-    req.open("PUT", url , true);
-    req.setRequestHeader("Content-type", "application/json");
-    req.send(JSON.stringify(tabs));
-  };
-
-  Client.getDevices = function getDevices(user, cb) {
-    var url = "/tabs/" + user;
-
-    var req = new XMLHttpRequest();
-
-    req.onreadystatechange=function() {
+    req.onreadystatechange = function() {
       if (req.readyState == 4) {
         if (req.status == 200) {
           cb(null, req.response);
@@ -53,30 +29,70 @@
       }
     };
 
-    req.open("GET", url , true);
+    req.open("GET", url, true);
     req.responseType = "json";
     req.send();
-  };
+  }
 
-  Client.getTabs = function getTabs(user, device, cb) {
-    var url = "/tabs/" + user + "/" + device;
-
+  // Not exported.
+  function _putJSON(url, data, cb) {
     var req = new XMLHttpRequest();
 
-    req.onreadystatechange=function() {
+    req.onreadystatechange = function onreadystatechange() {
       if (req.readyState == 4) {
         if (req.status == 200) {
-          var res = JSON.parse(req.response.tabs);
-          cb(null, res);
+          cb(null);
         } else {
           cb(req.status);
         }
       }
     };
 
-    req.open("GET", url , true);
-    req.responseType = "json";
-    req.send();
+    req.open("PUT", url, true);
+    req.setRequestHeader("Content-type", "application/json");
+    req.send(JSON.stringify(data));
+  };
+
+  Client.init = function init(options) {
+    // Use the injected AJAX functions if available, otherwise use our
+    // default XHR functions.
+    options = options || {};
+    this.getJSON = options.getJSON || _getJSON;
+    this.putJSON = options.putJSON || _putJSON;
+  };
+
+  Client.setTabs = function setTabs(user, device, data, cb) {
+    // Temporarily, tabs needs to be a string.
+    var tabs = { tabs: JSON.stringify(data) };
+
+    var url = "/tabs/" + user + "/" + device;
+
+    this.putJSON(url, tabs, cb);
+  };
+
+  Client.getDevices = function getDevices(user, cb) {
+    var url = "/tabs/" + user;
+
+    this.getJSON(url, cb);
+  };
+
+  Client.getTabs = function getTabs(user, device, cb) {
+    var url = "/tabs/" + user + "/" + device;
+
+    this.getJSON(url, function _cb(err, response) {
+      if (err) {
+        cb(err);
+        return;
+      }
+
+      // Temporarily, tabs returns a string.
+      try {
+        var res = JSON.parse(response.tabs);
+        cb(err, res);
+      } catch (e) {
+        cb(e);
+      }
+    });
   };
 
   // AMD / RequireJS
